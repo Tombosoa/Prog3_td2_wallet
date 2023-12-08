a33c80ab4dd56800cafd7f6895e64a034a048a39
Tombosoa <hei.nantenaina.4@gmail.com>
2023-12-08 15:33:16 +0300

    feat: make transaction between two account

--- src/main/java/com/example/td2wallet/functionnality/Functionnality.java
+++ src/main/java/com/example/td2wallet/functionnality/Functionnality.java
@@ -22,8 +22,9 @@ public class Functionnality {
     DataBaseConnection dbConnection = new DataBaseConnection(userName, password, databaseName);
     Connection conn = dbConnection.getConnection();
     public Statement statement;
-    public Account makeTransaction(Transaction transaction){
-        try{
+
+    public Account makeTransaction(Transaction transaction) {
+        try {
             String query = "INSERT INTO transaction (type,amount,account_id, label) VALUES ( ?, ?,?,?)";
             PreparedStatement insertStatement = conn.prepareStatement(query);
             insertStatement.setString(1, transaction.getType());
@@ -50,10 +51,10 @@ public class Functionnality {
 
             String updateAccount = "UPDATE account SET solde = ? WHERE id = ?";
             PreparedStatement newStatement = conn.prepareStatement(updateAccount);
-            if (transaction.getType().equals("Debit")){
+            if (transaction.getType().equals("Debit")) {
                 newStatement.setDouble(1, survol.getSolde() - transaction.getAmount());
                 newStatement.setInt(2, transaction.getAccount_id());
-            }else if(transaction.getType().equals("Credit")){
+            } else if (transaction.getType().equals("Credit")) {
                 newStatement.setDouble(1, survol.getSolde() + transaction.getAmount());
                 newStatement.setInt(2, transaction.getAccount_id());
             }
@@ -82,37 +83,126 @@ public class Functionnality {
             throw new RuntimeException(e);
         }
     }
+
     public static String transformSpaceToT(String originalDate) {
         return originalDate.replace(" ", "T");
     }
+
     public AccountDate getByDate(String transaction_date, int account_id) throws PropertyNotFoundException {
         try {
             String query = "SELECT transaction.id AS id, transaction_date, transaction.type as type, amount, account_id, label, solde, currency.name as name FROM account INNER JOIN transaction ON transaction.account_id = account.id INNER JOIN currency ON currency.id = account.currency_id WHERE transaction_date = ? AND account.id = ?";
             PreparedStatement preparedStatemente = conn.prepareStatement(query);
 
-                String transformedDate = transformSpaceToT(transaction_date);
-                OffsetDateTime dateTime = OffsetDateTime.parse(transformedDate);
+            String transformedDate = transformSpaceToT(transaction_date);
+            OffsetDateTime dateTime = OffsetDateTime.parse(transformedDate);
 
-                preparedStatemente.setTimestamp(1, Timestamp.from(dateTime.toInstant()));
-                preparedStatemente.setInt(2, account_id);
+            preparedStatemente.setTimestamp(1, Timestamp.from(dateTime.toInstant()));
+            preparedStatemente.setInt(2, account_id);
 
-                ResultSet resultSett = preparedStatemente.executeQuery();
-                    if (resultSett.next()) {
-                        Timestamp timestampFromResultSet = resultSett.getTimestamp("transaction_date");
-                        OffsetDateTime transacDate = OffsetDateTime.ofInstant(timestampFromResultSet.toInstant(), ZoneId.systemDefault());
-                        String type = resultSett.getString("type");
-                        double amount = resultSett.getDouble("amount");
-                        int account_Id = resultSett.getInt("account_id");
-                        String label = resultSett.getString("label");
-                        double solde = resultSett.getDouble("solde");
-                        String name = resultSett.getString("name");
-                        AccountDate account = new AccountDate(transacDate, type, amount, account_Id, label, solde, name);
-                        return  account;
-                    } else {
-                        throw new PropertyNotFoundException("Account not found");
-                    }
+            ResultSet resultSett = preparedStatemente.executeQuery();
+            if (resultSett.next()) {
+                Timestamp timestampFromResultSet = resultSett.getTimestamp("transaction_date");
+                OffsetDateTime transacDate = OffsetDateTime.ofInstant(timestampFromResultSet.toInstant(), ZoneId.systemDefault());
+                String type = resultSett.getString("type");
+                double amount = resultSett.getDouble("amount");
+                int account_Id = resultSett.getInt("account_id");
+                String label = resultSett.getString("label");
+                double solde = resultSett.getDouble("solde");
+                String name = resultSett.getString("name");
+                AccountDate account = new AccountDate(transacDate, type, amount, account_Id, label, solde, name);
+                return account;
+            } else {
+                throw new PropertyNotFoundException("Account not found");
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+
+    public TransferHistory makeTransfer(double montant, int idCompteDeb, int idCompteCred) {
+        try {
+            int idTransactionDeb = makeTransactionAct(montant, "Debit", idCompteDeb);
+            int idTransactionCred = makeTransactionAct(montant, "Credit", idCompteCred);
+
+            insertTransferHistory(idTransactionDeb, idTransactionCred);
+
+            TransferHistory transferHistory = new TransferHistory();
+            transferHistory.setId_transactionDeb(idTransactionDeb);
+            transferHistory.setId_transactionCred(idTransactionCred);
+
+            return transferHistory;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int makeTransactionAct(double amount, String action, int account_id) {
+        int idTransaction = 0;
+        try {
+            String query = "INSERT INTO transaction (type, amount, account_id, label) VALUES (?, ?, ?, ?)";
+            PreparedStatement insertStatement = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            insertStatement.setString(1, action);
+            insertStatement.setDouble(2, amount);
+            insertStatement.setInt(3, account_id);
+            insertStatement.setString(4, "make transfer");
+            insertStatement.executeUpdate();
+
+            ResultSet generatedKeys = insertStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                idTransaction = generatedKeys.getInt(1);
+
+                updateAccountBalance(account_id, action, amount);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return idTransaction;
+    }
+
+    private void insertTransferHistory(int idTransactionDeb, int idTransactionCred) throws SQLException {
+        String query = "INSERT INTO transferHistory (id_transactiondeb, id_transactioncred) VALUES (?, ?)";
+        try (PreparedStatement statementTransfer = conn.prepareStatement(query)) {
+            statementTransfer.setInt(1, idTransactionDeb);
+            statementTransfer.setInt(2, idTransactionCred);
+            statementTransfer.executeUpdate();
+        }
+    }
+
+    private void updateAccountBalance(int account_id, String action, double amount) throws SQLException {
+        Account account = getAccountById(account_id);
+
+        String updateAccount = "UPDATE account SET solde = ? WHERE id = ?";
+        try (PreparedStatement newStatement = conn.prepareStatement(updateAccount)) {
+            if (action.equals("Debit")) {
+                newStatement.setDouble(1, account.getSolde() - amount);
+            } else if (action.equals("Credit")) {
+                newStatement.setDouble(1, account.getSolde() + amount);
+            }
+            newStatement.setInt(2, account_id);
+            newStatement.executeUpdate();
+        }
+    }
+
+    private Account getAccountById(int account_id) throws SQLException {
+        String selectAccount = "SELECT * FROM account WHERE id = ?";
+        try (PreparedStatement selectStatement = conn.prepareStatement(selectAccount)) {
+            selectStatement.setInt(1, account_id);
+
+            try (ResultSet resultSet = selectStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    Account account = new Account();
+                    account.setAccount_id(resultSet.getInt("id"));
+                    account.setName(resultSet.getString("name"));
+                    account.setUser_id(String.valueOf(UUID.fromString(resultSet.getString("user_id"))));
+                    account.setCurrency_id(resultSet.getInt("currency_id"));
+                    account.setType(resultSet.getString("type"));
+                    account.setSolde(resultSet.getDouble("solde"));
+                    return account;
+                } else {
+                    throw new SQLException("Compte non trouvé avec l'ID : " + account_id);
+                }
+            }
         }
     }
 
