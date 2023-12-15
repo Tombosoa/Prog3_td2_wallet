cd5af5d6465283785092b349e56963f74647eb09
tokiniaina <hei.tokiniaina@gmail.com>
2023-12-15 15:09:03 +0300

    make some modification on the class Subcategory

--- src/main/java/com/example/td2wallet/Entity/SubCategory.java
+++ src/main/java/com/example/td2wallet/Entity/SubCategory.java
@@ -1,17 +1,28 @@
 package com.example.td2wallet.Entity;
 
-import com.example.td2wallet.Enum.SubCategoryEnum;
-
 public class SubCategory {
     private int id;
-    private SubCategoryEnum subcategory_name;
+    private String subcategory_name;
+    private int category_id ;
 
-    public SubCategory(int id, SubCategoryEnum subcategory_name) {
-        this.id = id;
-        this.subcategory_name = subcategory_name;
+
+
+    public int getCategory_id() {
+        return category_id;
     }
 
-    public SubCategory(SubCategoryEnum subcategory_name) {
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getSubcategory_name() {
+        return subcategory_name;
+    }
+
+    public SubCategory() {
+    }
+
+    public void setSubcategory_name(String subcategory_name) {
         this.subcategory_name = subcategory_name;
     }
 
@@ -23,19 +34,23 @@ public class SubCategory {
         this.id = id;
     }
 
-    public SubCategoryEnum getSubcategory_name() {
-        return subcategory_name;
+    public SubCategory(int id, String subcategory_name, int category_id) {
+        this.id = id;
+        this.subcategory_name = subcategory_name;
+        this.category_id = category_id;
     }
 
-    public void setSubcategory_name(SubCategoryEnum subcategory_name) {
+    public SubCategory(String subcategory_name, int category_id) {
         this.subcategory_name = subcategory_name;
+        this.category_id = category_id;
     }
 
     @Override
     public String toString() {
         return "SubCategory{" +
                 "id=" + id +
-                ", subcategory_name=" + subcategory_name +
+                ", subcategory_name='" + subcategory_name + '\'' +
+                ", category_id=" + category_id +
                 '}';
     }
 }
